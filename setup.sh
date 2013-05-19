9c87835716b1518df9721712b7ccc385843c1002
rvergis <ron.vergis@gmail.com>
2013-05-19 11:25:32 -0700

    Updated Lessons, new costumes

--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@
 export DIR="`pwd`"
 export SOFT_LINK="/Applications/Scratch 1.4/Media/Costumes/Scratchy"
 rm "$SOFT_LINK"
-ln -s "$DIR/Resources" "$SOFT_LINK"
+ln -s "$DIR/Resources/Costumes" "$SOFT_LINK"
+
+export SOFT_LINK="/Applications/Scratch 1.4/Media/Backgrounds/Scratchy"
+rm "$SOFT_LINK"
+ln -s "$DIR/Resources/Backgrounds" "$SOFT_LINK"
 
