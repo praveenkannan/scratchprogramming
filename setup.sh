e5954a962685d9ad9b41e14c2326d0065b59b444
rvergis <ron.vergis@gmail.com>
2013-05-26 01:20:48 -0700

    Unit-14, new audio clip

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ export SOFT_LINK="/Applications/Scratch 1.4/Media/Backgrounds/Scratchy"
 rm "$SOFT_LINK"
 ln -s "$DIR/Resources/Backgrounds" "$SOFT_LINK"
 
+export SOFT_LINK="/Applications/Scratch 1.4/Media/Sounds/Scratchy"
+ln -s "$DIR/Resources/Sounds" "$SOFT_LINK"
+
